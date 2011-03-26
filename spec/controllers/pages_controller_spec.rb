57e0c566c61fb616b2b8fc35a8da6e5b9c156881
Narendra Nag <narendranag@gmail.com>
2011-03-26 14:34:06 +0530

    updated the test suite

--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,26 +1,53 @@
 require 'spec_helper'
 
 describe PagesController do
+  
+  render_views
 
+
+  # Home Page
+  
   describe "GET 'home'" do
     it "should be successful" do
       get 'home'
       response.should be_success
     end
   end
+  
+  it "should have the right title" do
+    get 'home'
+    response.should have_selector("title",
+                      :content => " | Home")
+  end
 
+  # Contact Page
+  
   describe "GET 'contact'" do
     it "should be successful" do
       get 'contact'
       response.should be_success
     end
   end
+  
+  it "should have the right title" do
+    get 'contact'
+    response.should have_selector("title",
+                      :content => " | Contact")
+  end
 
+  # About Page
+  
   describe "GET 'about'" do
     it "should be successful" do
       get 'about'
       response.should be_success
     end
   end
-
+  
+  it "should have the right title" do
+    get 'about'
+    response.should have_selector("title",
+                      :content => " | About")
+  end
+  
 end
